d12c3da1849d1a4f87fad1fd145cb9cd36e2d134
OleksandrB93 <barabashsan4ik@gmail.com>
2023-05-12 12:25:21 +0300

    test

---
+++ app/auth/page.tsx
@@ -0,0 +1,48 @@
+"use client";
+import { useState } from "react";
+import Input from "@/components/Input/Input";
+
+const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
+      <div className="bg-black w-full h-full bg-opacity-50">
+        <nav className="px-12 py-12 ">
+          <img className="h-12" src="/images/logo.png" alt="Logo" />
+        </nav>
+        <div className="flex justify-center ">
+          <div className="bg-black bg-opacity-70 px-16 py-16  self-center mt-2  lg:w-2/5 lg:max-w-md rounded-md w-full ">
+            <h2 className="text-white text-4xl mb-8 font-semibold ">Sign in</h2>
+            <div className="flex flex-col gap-4">
+              <Input
+                id="name"
+                type="text"
+                label="Username"
+                value={name}
+                onChange={(e: any) => setName(e.target.value)}
+              />
+              <Input
+                id="email"
+                type="email"
+                label="Email address or phone number"
+                value={email}
+                onChange={(e: any) => setEmail(e.target.value)}
+              />
+              <Input
+                type="password"
+                id="password"
+                label="Password"
+                value={password}
+                onChange={(e: any) => setPassword(e.target.value)}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Auth;
